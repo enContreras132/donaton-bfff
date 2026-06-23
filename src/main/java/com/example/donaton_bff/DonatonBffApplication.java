856434388788c9376bcb8c207c175d9d1cce754d
Enzo <en.contrerase@duocuc.cl>
2026-06-23 02:43:20 -0400

    Initial commit

---
+++ src/main/java/com/example/donaton_bff/DonatonBffApplication.java
@@ -0,0 +1,13 @@
+package com.example.donaton_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonatonBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonatonBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/donaton_bff/DonatonBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.donaton_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonatonBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
